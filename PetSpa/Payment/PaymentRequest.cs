0e6f1cdc0378d15255bf298e61df790f42c6ac18
namlxpse173365@fpt.edu.vn <namlxpse173365@fpt.edu.vn>
2024-06-26 16:47:57 +0700

    Update Booking and Payment

--- PetSpa/Payment/PaymentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetSpa.Payment
-{
-    public class PaymentRequest
-    {
-        public string OrderId { get; set; }
-        public decimal Amount { get; set; }
-        public string OrderDescription { get; set; }
-    }
-}
